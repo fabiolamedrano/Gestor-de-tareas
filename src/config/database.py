c4dd4546902ebc852fd52adeb622cc434528b112
fabiolamedrano <fabiolamedrano1528@gmail.com>
2026-07-26 16:57:26 -0700

    acualize el requirements

--- src/config/database.py
+++ src/config/database.py
@@ -5,11 +5,9 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 
 load_dotenv()
 
-# Especifica la ruta local apuntando a la instancia Express
 DB_SERVER = "localhost\\SQLEXPRESS"  
-DB_DATABASE = "gestor_tareas"  # Reemplaza con el nombre real de tu BD
+DB_DATABASE = "gestor_tareas"
 
-# La URL limpia sin el puerto 1433
 DATABASE_URL = (
     f"mssql+pyodbc://@{DB_SERVER}/{DB_DATABASE}"
     f"?driver=ODBC+Driver+17+for+SQL+Server&trusted_connection=yes"
